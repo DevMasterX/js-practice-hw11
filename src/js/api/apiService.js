0b0bcc8c9b1028988d8049fff10adb98a9b469f2
DevMasterX <sokol080288@gmail.com>
2025-01-20 00:21:28 +0200

    546514

--- src/js/api/apiService.js
+++ src/js/api/apiService.js
@@ -2,10 +2,32 @@ import axios from 'axios';
 
 const API_KEY = '39861298-38e1e10cd9c08fce6ef5c3957';
 axios.defaults.baseURL = 'https://pixabay.com/api/';
-let page = 1;
+
 const params = new URLSearchParams({
   key: API_KEY,
-  image_type: photo,
-  orientation: horizontal,
+  image_type: 'photo',
+  orientation: 'horizontal',
   safesearch: true,
 });
+
+class ApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.PER_PAGE = 40;
+  }
+
+  async fetchPictures() {
+    try {
+    } catch (error) {}
+  }
+}
+
+// ---------пример запроса
+// axios({
+//   method: 'get',
+//   url: '/user',
+//   params: {
+//     ID: 12345,
+//   },
+// });
