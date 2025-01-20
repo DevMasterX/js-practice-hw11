7ef3ccfafd41aa1cd06dccea2643b9852477700f
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2025-01-20 16:20:37 +0200

    5346541651

--- src/index.js
+++ src/index.js
@@ -56,13 +56,16 @@ function renderGallery(items) {
       ${likes}
     </p>
     <p class="info-item">
-      <b>Views</b> ${views}
+      <b>Views</b>
+      ${views}
     </p>
     <p class="info-item">
-      <b>Comments</b>${comments}
+      <b>Comments</b>
+      ${comments}
     </p>
     <p class="info-item">
-      <b>Downloads</b>${downloads}
+      <b>Downloads</b>
+      ${downloads}
     </p>
   </div>
 </div>
