c950012f483859b7c2af56ec24cacf9d1b31c93d
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2025-01-20 15:08:52 +0200

    89984

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import Notiflix from 'notiflix';
 import './js/api/apiService';
+import { fetchPictures } from './js/api/utils/fetchPictures';
 import { ApiService } from './js/api/apiService';
 
 const form = document.querySelector('.js-form');
+const loadMoreBtn = document.querySelector('.js-load-more');
+const gallery = document.querySelector('.js-gallery');
 
 const apiService = new ApiService();
 
 form.addEventListener('submit', onFormSubmit);
 
-function onFormSubmit(event) {
+async function onFormSubmit(event) {
   event.preventDefault();
 
   apiService.query = event.currentTarget.elements.searchQuery.value.trim();
-  form.reset();
+
   if (apiService.query === '') {
     Notiflix.Notify.warning('Fill in the input field!!!', {
       //   width: '30%',
@@ -22,23 +25,50 @@ function onFormSubmit(event) {
     });
     return;
   }
+  form.reset();
 
-  fetchPictures();
+  const pictures = await fetchPictures(apiService);
+
+  const { hits } = pictures;
+  console.log('🚀  hits:', hits);
+
+  renderGallery(hits);
 }
 
-async function fetchPictures() {
-  const result = await apiService.fetchPictures();
-  const { hits, totalHits } = result;
-  console.log('🚀  totalHits:', totalHits);
-  console.log('🚀  hits:', hits);
-  if (!hits.length) {
-    Notiflix.Notify.failure(
-      '❌ Sorry, there are no images matching your search query. Please try again.',
-      {
-        clickToClose: true,
-        position: 'center-center',
-      }
-    );
-    return;
-  }
+function renderGallery(items) {
+  const galleryMarkup = items
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+  
+  <div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+      ${likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b> ${views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b>${comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>${downloads}
+    </p>
+  </div>
+</div>
+  `
+    )
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', galleryMarkup);
 }
